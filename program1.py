ee289c1ee6af0a2e17af840d28063ee3625f1692
satyam_2604 <satyamagrawalv2604@gmail.com>
2024-10-07 16:02:09 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
 
+        for char in s:
+            if char in bracket_map.values():  # open bracket
+                stack.append(char)
+            elif char in bracket_map.keys():  # close bracket
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack
