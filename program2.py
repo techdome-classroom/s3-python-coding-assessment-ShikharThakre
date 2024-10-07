5a6b16441e8689f0e591e476097a307ab6d567ea
satyam_2604 <satyamagrawalv2604@gmail.com>
2024-10-07 16:01:55 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        if not s:  # <--- Add this check
+            return 0
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        for i in range(len(s) - 1):
+            if roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+        return total + roman_map[s[-1]]
